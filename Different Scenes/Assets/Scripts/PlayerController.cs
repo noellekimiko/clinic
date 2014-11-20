20a31ab529750bbbc850362965b57222c3bbc05e
Noelle Fa-Kaji <noelle.fa-kaji@liferay.com>
2014-11-20 13:01:26 -0800

    Loading Time tests: project with 2 models, 2 scenes
    Uses birch and libra complex models, but each model is in its own scene

---
+++ Different Scenes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	
+	void FixedUpdate () {
+		
+		Vector3 movement = new Vector3(Input.gyro.gravity.x, 0.0f, Input.gyro.gravity.y);
+		Quaternion direction = Quaternion.Euler (0.0f, transform.rotation.y, 0.0f);
+		movement = direction * movement;
+		
+		rigidbody.AddForce(movement * speed * Time.deltaTime); //omitting second argument
+	}
+	
+	void OnGUI () 
+	{
+		GUI.Box(new Rect(20,20,300,200), "Scenes");
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(25,45,290,80), "Birch")) 
+		{
+			Application.LoadLevel(0);
+		}
+		
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(25,130,290,80), "Libra Complex")) 
+		{
+			Application.LoadLevel(1);
+		}
+	}
+}
