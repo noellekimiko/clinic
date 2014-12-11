f8f619711d7a74f1bf62cbbe66f3b65000f96dde
Noelle Fa-Kaji <noelle.fa-kaji@liferay.com>
2014-12-10 20:25:01 -0800

    Updated gyroscope player controller to use GameState architecture
    Code is now the same as in the KeyPressPlayerController, but with input data from the gyroscope instead of keypresses

--- States with Ball/Assets/Scripts/PlayerController.cs
+++ States with Ball/Assets/Scripts/PlayerController.cs
@@ -4,59 +4,40 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	public float speed;
-	public GUIText countText;
-	public GUIText gameOverText;
-	private int score;
-	private int pickupCount;
-
-	public Pickups pickups;
-
-	void Start ()
-	{
-		score = 0;
-		pickupCount = 0;
-		SetScoreText();
-		gameOverText.text = "";
-	}
 
 	void FixedUpdate () {
-
+		
 		Vector3 movement = new Vector3(Input.gyro.gravity.x, 0.0f, Input.gyro.gravity.y);
 		Quaternion direction = Quaternion.Euler (0.0f, transform.rotation.y, 0.0f);
 		movement = direction * movement;
-
+		
 		rigidbody.AddForce(movement * speed * Time.deltaTime); //omitting second argument
 	}
 	
 	void OnTriggerEnter(Collider other) 
 	{
-		
 		if (other.gameObject.tag == "PickUp") 
 		{
 			other.gameObject.SetActive(false);
-			score += 1;
-			pickupCount += 1;
-			SetScoreText();
+			GameState.Instance.AddToInventory(other.gameObject.tag);
+			LevelGUI.Instance.SetScoreText(+1);
 		}
-
+		
 		if (other.gameObject.tag == "Avoid") 
 		{
 			other.gameObject.SetActive(false);
-			score -= 1;
-			SetScoreText();
+			GameState.Instance.AddToInventory(other.gameObject.tag);
+			LevelGUI.Instance.SetScoreText(-1);
 		}
-	}
-
-	void SetScoreText () 
-	{
-		countText.text = "Score: " + score.ToString();
-		SetGameOverText();
-	}
-
-	void SetGameOverText () {
-		if (pickupCount >= pickups.numPickups) 
+		
+		if (other.gameObject.tag == "Special") 
 		{
-			gameOverText.text = "Game Over!";
+			if (GameState.Instance.CheckInventory("PickUp", 3)) 
+			{
+				other.gameObject.SetActive(false);
+				GameState.Instance.AddToInventory(other.gameObject.tag);
+				LevelGUI.Instance.SetScoreText(+5);
+			}
 		}
 	}
 }
